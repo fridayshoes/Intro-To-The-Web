491400a6eebd646f73715177e42670665311f4e1
fridayshoes <one_amp@hotmail.com>
2022-05-30 16:58:02 +0100

    Sinatra: Introducing params

--- myapp.rb
+++ myapp.rb
@@ -17,7 +17,13 @@ get '/thesweetshop' do
   'Rhubarb & Custard anyone?'
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Misty", "Almond"].sample
+  erb :index
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb :index
 end
