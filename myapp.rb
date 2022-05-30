ce341853a0db183d6d8b157a4e4f88143f8130ff
fridayshoes <one_amp@hotmail.com>
2022-05-30 15:21:11 +0100

    Sinatra setup through to cat image

---
+++ myapp.rb
@@ -0,0 +1,24 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+
+get '/' do
+  'Hello world!'
+end
+
+get '/secret' do
+  'Here is the password'
+end
+
+get '/area51' do
+  'Aliens do not exist!'
+end
+
+get '/thesweetshop' do
+  'Rhubarb & Custard anyone?'
+end
+
+get '/cat' do
+  "<div>
+  <img src='http://placekitten.com/500/500'>
+  </div>"
+end
