bfe4d5fc736cc36f6b3abc1c58011f39ced99afe
fridayshoes <one_amp@hotmail.com>
2022-05-30 16:08:59 +0100

    Sinatra: Keeping views clean

--- myapp.rb
+++ myapp.rb
@@ -18,7 +18,6 @@ get '/thesweetshop' do
 end
 
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-  <img src='http://placekitten.com/500/500'>
-  </div>"
+  @name = ["Amigo", "Misty", "Almond"].sample
+  erb :index
 end
